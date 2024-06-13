c90c51f652a84920bfe13ac40099acdf83a01197
jjobe5989 <s3thjobe@gmail.com>
2024-06-12 21:00:01 -0500

    added typewriter effect to main page and a photo with css for both

--- my-app/src/components/Main.jsx
+++ my-app/src/components/Main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { TypeAnimation } from 'react-type-animation';
 import videoBG from "../assets/ReefBackground.mp4"
+import Photo from "../assets/Seth.jpg"
 
 const Main = () => {
     return (
@@ -7,10 +9,28 @@ const Main = () => {
             <div className="overlay"></div>
             <video src={videoBG} autoPlay loop muted/>
             <div className="content">
-                <h1>Welcome</h1>
+            <TypeAnimation
+                sequence={[
+                    'Hi',
+                    1000,
+                    'My Name Is Jonathan.',
+                    1000,
+                    'I am a Junior Frontend Web Developer from Middle Tennessee.',
+                    1000,
+                    'Welcome to my Portfolio.',
+                    1000
+                ]}
+                    wrapper="span"
+                    speed={30}
+                    style={{ fontSize: '2em', display: 'inline-block' }}
+                    repeat={Infinity}
+            />
+            <div className="Photo">
+                <img src={Photo} alt="Avatar"/>
+            </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Main;
